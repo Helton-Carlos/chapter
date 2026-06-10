ba2ed27bb10aa3b15f5ccb172566caa1b04dd738
Helton Silva <helton.silva@melhorenvio.com>
2026-06-10 14:22:18 -0300

    feat: add new pages

--- react-full/react-full/src/App.tsx
+++ react-full/react-full/src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route, Link } from 'react-router';
 import {Home} from '@/pages/Home';
+import {About} from '@/pages/About';
+import {Contact} from '@/pages/Contact';
 import { NeHeader } from '@/layout/NeHeader';
 
 export default function App() {
@@ -22,6 +24,8 @@ export default function App() {
     <div className='p-8'>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
     </>
