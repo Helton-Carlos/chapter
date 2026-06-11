ea26cf95b768c16c6a3308c26ff242397900a0fa
Helton Silva <helton.silva@melhorenvio.com>
2026-06-11 09:13:46 -0300

    feat: add sections

--- react-full/react-full/src/pages/Home.tsx
+++ react-full/react-full/src/pages/Home.tsx
@@ -3,12 +3,33 @@ import { NeButton } from '@/components/NeButton';
 
 export function Home() {
   return (
-    <div className='container'>
-      <main className='bg-[url(https://www.abc.med.br/fmfiles/index.asp/::abcmed::/abc/ferroada-de-abelha.jpg)] bg-cover bg-center h-70 flex items-center justify-center text-white'>
-        <h2>Seja-bem vindo ao BeeSting</h2>
-      </main>
+    <>
+      <div className='container'>
+        <main className='bg-[url(https://www.abc.med.br/fmfiles/index.asp/::abcmed::/abc/ferroada-de-abelha.jpg)] bg-cover bg-center h-70 flex items-center justify-center text-white'>
+          <h2>Seja-bem vindo ao BeeSting</h2>
+        </main>
+        
+        <section className='py-8'>
+          <h2 className='text-2xl font-bold mb-4'>Tipos de abelhas</h2>
+
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+            {sectionHome.map((section, index) => (
+              <div key={index} className='bg-white rounded-lg shadow-md overflow-hidden'>
+                <img src={section.image} alt={section.title} className='w-full object-cover' />
+
+                <div className='p-4'>
+                  <h3 className='text-xl font-semibold mb-2'>{section.title}</h3>
+                  <p className='text-gray-700 mb-4'>{section.description}</p>
+                  <NeButton>Saiba mais</NeButton>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section> 
+      </div>
       
-      <section className='py-8'>
+      <section className='bg-primary-500 w-full p-8'>
+       <div className='container text-center text-white'>
         <h2 className='text-2xl font-bold mb-4'>Tipos de abelhas</h2>
 
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
@@ -24,7 +45,26 @@ export function Home() {
             </div>
           ))}
         </div>
-      </section>  
-    </div>
+       </div>
+      </section> 
+
+      <section className='py-8 container'>
+        <h2 className='text-2xl font-bold mb-4'>Tipos de abelhas</h2>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+          {sectionHome.map((section, index) => (
+            <div key={index} className='bg-white rounded-lg shadow-md overflow-hidden'>
+              <img src={section.image} alt={section.title} className='w-full object-cover' />
+
+              <div className='p-4'>
+                <h3 className='text-xl font-semibold mb-2'>{section.title}</h3>
+                <p className='text-gray-700 mb-4'>{section.description}</p>
+                <NeButton>Saiba mais</NeButton>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section> 
+    </>
   )
 }
