d7f81a63b766cdcef74ab1838b4309f863b7e551
Helton Silva <helton.silva@melhorenvio.com>
2026-06-11 15:12:18 -0300

    fix: remove space in tags

--- react-full/react-full/src/__tests__/pages/Home.spec.tsx
+++ react-full/react-full/src/__tests__/pages/Home.spec.tsx
@@ -4,19 +4,19 @@ import { Home } from '@/pages/Home';
 
 describe('Render Home', () => {
   it('Render the application component', () => {
-    render( <Home />);
+    render(<Home />);
 
     expect(screen).toBeDefined();
   });
 
   it('Should title of page', () => {
-    const wrapper = render( <Home />);
+    const wrapper = render(<Home />);
 
     expect(wrapper.getByText('Seja-bem vindo ao BeeSting')).toBeInTheDocument();
   });
 
   it('Should testing sections title', () => {
-    const wrapper = render( <Home />);
+    const wrapper = render(<Home />);
 
     const titleTypes = wrapper.getByTestId('title-types-bees');
     const titleCuriosities = wrapper.getByTestId('title-curiosities')
