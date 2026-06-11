fd74395a462b48903dda0cca1ed9924b5407a681
Helton Silva <helton.silva@melhorenvio.com>
2026-06-11 09:46:28 -0300

    feat: add info in pages

--- react-full/react-full/src/pages/About.tsx
+++ react-full/react-full/src/pages/About.tsx
@@ -1,7 +1,17 @@
 export function About() {
   return (
-    <>
+    <div className='container'>
       <h2>Sobre</h2>
-    </>
+
+      <h4 className='text-lg font-bold py-4'>Bem-vindo à página Sobre!</h4>
+
+      <p>O BeeSting é um projeto dedicado a fornecer informações sobre as abelhas, suas espécies, comportamentos e a importância que desempenham no ecossistema. Nosso objetivo é educar e conscientizar as pessoas sobre a importância da conservação das abelhas e como elas contribuem para a polinização de plantas, produção de alimentos e manutenção da biodiversidade.</p>
+
+      <p>Na página Sobre, você encontrará informações detalhadas sobre as diferentes espécies de abelhas, seus habitats, comportamentos e curiosidades. Também abordaremos a importância das abelhas para a agricultura, a produção de mel e outros produtos apícolas, além de destacar os desafios que as abelhas enfrentam atualmente, como a perda de habitat, o uso de pesticidas e as mudanças climáticas.</p>
+
+      <p>Nosso compromisso é fornecer informações precisas e atualizadas sobre as abelhas, promovendo a conscientização sobre a importância da conservação dessas incríveis criaturas. Junte-se a nós nessa jornada para aprender mais sobre as abelhas e como podemos protegê-las para garantir um futuro sustentável para todos.</p>
+  
+      <img src='https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YWJlbGFzfGVufDB8fDB8fHww&w=1000&q=80' alt='Abelha' className='w-full h-auto mt-4 rounded-lg shadow-md' />  
+    </div>
   )
 }
--- react-full/react-full/src/pages/Contact.tsx
+++ react-full/react-full/src/pages/Contact.tsx
@@ -1,7 +1,15 @@
 export function Contact() {
   return (
-    <>
+    <div className='container'>
       <h2>Contato</h2>
-    </>
+
+      <ul>
+        <li>Email: contato@beesting.com</li>
+        <li>Telefone: (11) 1234-5678</li>
+        <li>Endereço: Rua das Abelhas, 123 - São Paulo/SP</li>  
+      </ul>
+
+      <img src='https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YWJlbGFzfGVufDB8fDB8fHww&w=1000&q=80' alt='Abelha' className='w-full h-auto mt-4 rounded-lg shadow-md' />
+    </div>
   )
 }
